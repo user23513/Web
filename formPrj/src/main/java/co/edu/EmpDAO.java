743317c392f451d40d1d745bd30a398013888f64
soo <soo@gmail.com>
2022-06-08 16:13:32 +0900

    form

---
+++ formPrj/src/main/java/co/edu/EmpDAO.java
@@ -0,0 +1,52 @@
+package co.edu;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmpDAO extends DAO {
+	
+	// class => 복합적인 데이터를 하나의 변수에다가.
+	//사원번호, 이름, 이메일, 직무 ... 이런것을 하나의 변수에 담을수 없기 때문에 -> calss작성 : 필드로 선언.
+//	public void insertEmp(int eId, String name, String email,String job) {
+//	하나하나 매개변수에 적어줘야한다. 
+//	}
+	public void insertEmp(Employee emp) {
+		getConnect();
+		String sql = "insert into employees (employee_id, last_name, email, job_id, hire_date) values(employees_seq.nextval,?,?,?,?)";
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, emp.getLastName());
+			psmt.setString(2, emp.getEmail());
+			psmt.setString(3, emp.getJobId());
+			psmt.setString(4, emp.getHireDate());
+			int r = psmt.executeUpdate();
+			System.out.println(r+"건 입력.");
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disConnect();
+		}
+		
+	}
+	
+	public List<String> getNames() {
+		getConnect();
+		String sql = "select first_name from employees";
+		List<String> list = new ArrayList<>();
+		try {
+			psmt = conn.prepareStatement(sql);
+			rs = psmt.executeQuery();
+			while (rs.next()) {
+				list.add(rs.getString("first_name"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disConnect();
+		}
+		return list;
+	}
+
+}
---
+++ formPrj/src/main/java/co/edu/Employee.java
@@ -0,0 +1,46 @@
+package co.edu;
+
+public class Employee {
+	
+	//필드
+	int empId;
+	String lastName;
+	String email;
+	String jobId;
+	String hireDate;
+	
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getJobId() {
+		return jobId;
+	}
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+	public String getHireDate() {
+		return hireDate;
+	}
+	public void setHireDate(String hireDate) {
+		this.hireDate = hireDate;
+	}
+	
+	
+
+	
+}
