c6dd275417de1a6014fab431b29a491c0492e8f2
soo <soo@gmail.com>
2022-06-24 16:47:18 +0900

    220624 commit

--- formPrj/src/main/java/co/edu/DAO.java
+++ formPrj/src/main/java/co/edu/DAO.java
@@ -8,14 +8,14 @@ import java.sql.SQLException;
 
 public class DAO { 
 
-	Connection conn;
-	PreparedStatement psmt;
-	ResultSet rs;
+	public Connection conn;
+	public PreparedStatement psmt; // 쿼리문 실행하고 결과를 받아오는
+	public ResultSet rs; // 결과값을 여기다 담아준다.
 
 	public void getConnect() {
 		try { 
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "hr"); // connection객체가 만들어짐
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "scott", "tiger"); // connection객체가 만들어짐
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
--- formPrj/src/main/java/co/edu/EmpDAO.java
+++ formPrj/src/main/java/co/edu/EmpDAO.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EmpDAO extends DAO {
-	
+
 	// class => 복합적인 데이터를 하나의 변수에다가.
-	//사원번호, 이름, 이메일, 직무 ... 이런것을 하나의 변수에 담을수 없기 때문에 -> calss작성 : 필드로 선언.
+	// 사원번호, 이름, 이메일, 직무 ... 이런것을 하나의 변수에 담을수 없기 때문에 -> calss작성 : 필드로 선언.
 //	public void insertEmp(int eId, String name, String email,String job) {
 //	하나하나 매개변수에 적어줘야한다. 
 //	}
@@ -21,16 +21,16 @@ public class EmpDAO extends DAO {
 			psmt.setString(3, emp.getJobId());
 			psmt.setString(4, emp.getHireDate());
 			int r = psmt.executeUpdate();
-			System.out.println(r+"건 입력.");
-			
+			System.out.println(r + "건 입력.");
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			disConnect();
 		}
-		
+
 	}
-	
+
 	public List<String> getNames() {
 		getConnect();
 		String sql = "select first_name from employees";
@@ -40,6 +40,33 @@ public class EmpDAO extends DAO {
 			rs = psmt.executeQuery();
 			while (rs.next()) {
 				list.add(rs.getString("first_name"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disConnect();
+		}
+		return list;
+	}
+
+	public List<Employee> empList() {
+		getConnect();
+		String sql = "select * from employees";
+		List<Employee> list = new ArrayList<>();
+		
+		try {
+			psmt = conn.prepareStatement(sql); // 쿼리를 실행하고 받아오는 
+			rs = psmt.executeQuery(); // 실제 실행
+			while (rs.next()) { // 한건씩 읽어오겠다.
+				Employee emp= new Employee();
+				emp.setEmail(rs.getString("email"));
+				emp.setEmpId(rs.getInt("employee_id"));
+				emp.setHireDate(rs.getString("hire_date"));
+				emp.setJobId(rs.getString("job_id"));
+				emp.setLastName(rs.getString("last_name"));
+				
+				
+				list.add(emp);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
