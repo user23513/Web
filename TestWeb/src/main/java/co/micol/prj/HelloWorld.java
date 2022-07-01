a78774ffdd0c5d46b618ed83aca56d632d1e870e
soo <soo@gmail.com>
2022-07-01 17:56:01 +0900

    20220701 commit

---
+++ TestWeb/src/main/java/co/micol/prj/HelloWorld.java
@@ -0,0 +1,26 @@
+package co.micol.prj;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet("/HelloWorld") // 요이름으로 서블릿컨테이너에 등록해라
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public HelloWorld() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
