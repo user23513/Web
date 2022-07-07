55fcf05628932b87eb0f56802767765dffdddb7f
soo <soo@gmail.com>
2022-07-07 14:11:43 +0900

    220707

--- TestWeb/src/main/java/co/micol/prj/comm/DAO.java
+++ TestWeb/src/main/java/co/micol/prj/comm/DAO.java
@@ -14,28 +14,15 @@ public class DAO {
 
 	public void getConnect() {
 		try { 
-			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Class.forName("oracle.jdbc.driver.OracleDriver"); // 드라이버가 있어야 디비랑 소통할수있다. 앱이 바로 디비랑 소통을 못함. 앱에 드라이버가 로드됨
 			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "hr"); // connection객체가 만들어짐
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void disConnect() {
-		if (conn != null) {
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		if(psmt != null) {
-			try {
-				psmt.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+	public void disConnect() { // 오픈한 순서의 반대로 닫는다
+		
 		if(rs != null) {
 			try {
 				rs.close();
@@ -43,6 +30,24 @@ public class DAO {
 				e.printStackTrace();
 			}
 		}
+		
+		if(psmt != null) {
+			try {
+				psmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
 
 	}
 
